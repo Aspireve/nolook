d406b59aa8e27675e9f089c8da620f545f4aed15
Steve Fernandes <93852415+Aspireve@users.noreply.github.com>
2024-07-27 13:57:04 +0530

    finalupload
    nolook

--- script.js
+++ script.js
@@ -12,24 +12,21 @@ function calculateTimeRemaining() {
     const now = new Date();
     const targetTime = new Date();
     targetTime.setHours(14, 30, 0, 0); // Set target time to 14:00:00
-    
-    if (now > targetTime) {
-        // If current time is past 14:00, set target time to 14:00 next day
-        targetTime.setDate(targetTime.getDate() + 1);
-    }
 
     const timeDiff = targetTime - now;
     return Math.floor(timeDiff / 1000); // Convert milliseconds to seconds
 }
 
 function runOnTimeReached() {
-    alert('It is now 14:00 PM!');
+    window.location.href = 'finaltime.html';
 }
 
 function startCountdown() {
     if (countdown) clearInterval(countdown);
     const updateCountdown = () => {
         const timeRemaining = calculateTimeRemaining();
+
+        console.log(timeRemaining)
         if (timeRemaining <= 0) {
             timeDisplay.textContent = '00:00';
             clearInterval(countdown);
@@ -41,6 +38,11 @@ function startCountdown() {
 
     updateCountdown(); // Update immediately
     countdown = setInterval(updateCountdown, 1000); // Update every second
+}
+
+function displaymap() {
+    console.log("hi")
+    window.location.href = "https://maps.app.goo.gl/EVHrXsA2btxURqjJ6"
 }
 
 startCountdown()
